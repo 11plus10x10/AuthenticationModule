24695637ecfda106c1dbb004a9c399662e6f076f
Alexandr Iurashchuk <a.iurashchuk@gmail.com>
2023-08-28 02:03:21 +0200

    clean up for mat-tab-nav-bar

--- AuthenticationModule/Client/authentication-module-client/src/app/app.component.ts
+++ AuthenticationModule/Client/authentication-module-client/src/app/app.component.ts
@@ -4,23 +4,30 @@ import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { RegisterComponent } from "./register/register.component";
-import { RouterLink, RouterOutlet } from "@angular/router";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { MatCardModule } from "@angular/material/card";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTabsModule } from "@angular/material/tabs";
+import { ILinks } from "./interfaces/ILinks";
 
 @Component({
   selector: 'au-root',
   standalone: true,
-  imports: [CommonModule, MatSlideToggleModule, MatFormFieldModule, MatInputModule, RegisterComponent, RouterOutlet, MatCardModule, MatToolbarModule, MatTabsModule, RouterLink],
+  imports: [CommonModule, MatSlideToggleModule, MatFormFieldModule, MatInputModule, RegisterComponent, RouterOutlet, MatCardModule, MatToolbarModule, MatTabsModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'authentication-module-client';
-  links = [
-    { linkName: "Sign Up", routeLink: "/signup" },
-    { linkName: "Sign In", routeLink: "/signin" },
-  ];
-  activeLink = this.links[0].linkName;
+  links: ILinks = {
+    "signup": {
+      linkName: "Sign Up",
+      routeLink: "/signup"
+    },
+    "signin": {
+      linkName: "Sign In",
+      routeLink: "/signin"
+    }
+  }
+  activeLink = "";
 }
---
+++ AuthenticationModule/Client/authentication-module-client/src/app/interfaces/ILinks.ts
@@ -0,0 +1,5 @@
+export interface ILinks {
+  [segment: string] : {
+    linkName: string, routeLink: string
+  }
+}
